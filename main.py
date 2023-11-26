8def11f162d2c9e4ad3c213ebc240a602d382a2f
Grigory-Yunusov <grigoriyzawr@gmail.com>
2023-11-26 04:42:38 +0300

    Grigoriy_Commit

---
+++ main.py
@@ -0,0 +1,113 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def validate(self):
+        pass
+
+class Name(Field):
+
+    def __init__(self, value, name):
+        super().__init__(value)
+        self.name = name
+
+
+class Phone(Field):
+    def __init__(self, phone, value):
+        super().__init__(value)
+        self.phone = phone
+
+    def validate(self):
+        if self.value and not (isinstance(self.phone, str) and len(self.phone) == 10 and self.phone.isdigit()):
+            raise ValueError("Phone must be a 10-digit number.")
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone_field = Phone(phone)
+        phone_field.validate()
+        self.phones.append(phone_field)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+        # for p in self.phones:
+        #     if p.value == phone:
+        #         self.phones.remove(p)
+        #         break
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                break
+
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        # return f"{phone} - not on the list!!!"
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, term):
+        result = []
+        for record_name, record in self.data.items():
+            if term.lower() in record_name.lower():
+                result.append(record)
+        return result
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+
+   # Створення нової адресної книги
+book = AddressBook()
+
+    # Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+for name, record in book.data.items():
+    print(record)
+
+    # Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+book.delete("Jane")
